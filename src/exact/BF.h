736d9a44629e2e6918f0904bc4014890f39807df
Pedro Henrique <pedro00dk@gmail.com>
2017-09-28 20:41:48 -0300

    Fix BF printing bug

--- src/exact/BF.h
+++ src/exact/BF.h
@@ -12,7 +12,7 @@ private:
     vector<string> patterns;
 
 public:
-    BF() = default;;
+    BF() = default;
 
     void setPatterns(vector<string> patterns) override {
         this->patterns = patterns;
@@ -24,7 +24,7 @@ public:
             auto patternLength = (int) pattern.length();
             int currentLineStart = 0;
             bool printCurrentLine = false;
-            for (int i = 0; i < textLength; i++) {
+            for (int i = 0; i <= textLength - patternLength; i++) {
                 int j = 0;
                 while (j < patternLength && text[i + j] == pattern[j]) j += 1;
                 if (j == patternLength) {
@@ -32,7 +32,10 @@ public:
                     if (print) printCurrentLine = true;
                 }
                 if (text[i + j] == '\n' || text[i + j] == '\r') {
-                    if (printCurrentLine) printf("%*s\n", i + j - 1, text + currentLineStart);
+                    if (printCurrentLine) {
+                        printf("%.*s\n", i + j - currentLineStart, text + currentLineStart);
+                        printCurrentLine = false;
+                    }
                     i += j;
                     currentLineStart = i + 1;
                 }
@@ -46,13 +49,17 @@ public:
         for (auto &pattern  : patterns) {
             auto patternLength = (int) pattern.length();
             for (string line; getline(text, line);) {
+                bool alreadyPrint = false;
                 auto lineLength = static_cast<int>(line.length());
-                for (int i = 0; i < lineLength - patternLength + 1; i++) {
+                for (int i = 0; i <= lineLength - patternLength; i++) {
                     int j = 0;
                     while (j < patternLength && line[i + j] == pattern[j]) j++;
                     if (j == patternLength) {
                         occurrences += 1;
-                        if (print) printf("%s\n", line.c_str());
+                        if (!alreadyPrint && print) {
+                            printf("%s\n", line.c_str());
+                            alreadyPrint = true;
+                        }
                         if (!count) continue;
                     }
                 }

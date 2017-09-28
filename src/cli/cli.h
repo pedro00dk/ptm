bc8ad258f8fe153f385d8b61932ec1e57c24b237
Pedro Henrique <pedro00dk@gmail.com>
2017-09-28 18:42:58 -0300

    Cli now parses the entire command and patterns file

--- src/cli/cli.h
+++ src/cli/cli.h
@@ -1,32 +1,32 @@
 #pragma once
 
 #include <iostream>
+#include <fstream>
 #include <vector>
 #include <getopt.h>
 
 using namespace std;
 
 // Cli options holder
-struct CliOptions
-{
+struct CliOptions {
     string algorithm = "";
     bool isExact = false;
     bool isApprox = false;
     int editDistance = -1;
     bool count = false;
-    string patternFile = "";
-    vector<string> arguments;
+    vector<string> patterns;
+    vector<string> files;
 };
 
 // CLI options
 static const string shortCliOptions = "ha:e:cp:";
 static const vector<option> longCliOptions = {
-    {"help", no_argument, nullptr, 'h'},
-    {"algorithm", required_argument, nullptr, 'a'},
-    {"edit", required_argument, nullptr, 'e'},
-    {"count", no_argument, nullptr, 'c'},
-    {"pattern", required_argument, nullptr, 'p'},
-    {nullptr, 0, nullptr, 0}};
+        {"help",      no_argument,       nullptr, 'h'},
+        {"algorithm", required_argument, nullptr, 'a'},
+        {"edit",      required_argument, nullptr, 'e'},
+        {"count",     no_argument,       nullptr, 'c'},
+        {"pattern",   required_argument, nullptr, 'p'},
+        {nullptr, 0,                     nullptr, 0}};
 
 // Options arguments
 static const vector<string> exactAlgorithmArguments = {"bf", "kmp", "bm", "ac", "so"};
@@ -34,129 +34,129 @@ static const vector<string> approxAlgorithmArguments = {"se", "uk", "wu"};
 
 // Help message
 static const string help =
-    "Usage:\n"
-    "ptm [<option>] pattern textfile [textfile...]  ->  search files\n"
-    "ptm [<option>] pattern  ->  search input stream\n"
-    "ptm <option> <-h | --help>  ->  show global help\n"
-    "ptm <-h | --help>  -> show option help\n"
-    "Options:\n"
-    "-h,--help            Display this information\n"
-    "-a,--algorithm <arg> Specify the search algorithm\n"
-    "-e,--edit <arg>      Specify the edit distance (required) for approx algorithms\n"
-    "-c,--count           Display only the number of found occurrences\n"
-    "-p,--pattern <arg>   Specify a file with patterns to search\n"
-    "                     If specified, the first global argument (pattern) is treated text file\n"
-    "\n"
-    "Examples:\n"
-    "ptm -algorithm kmp -c abra abracadrabra.txt\n"
-    "\n"
-    "\n"
-    "-a,--algorithm <arg> Specify the search algorithm\n"
-    "arg options:\n"
-    "\texact: bf, kmp, bm, ac, so\n"
-    "\tapprox: se, uk, wu\n"
-    "\n"
-    "If algorithm is not specified:\n"
-    "\tIf nothing (except -c,--count) is specified, \"bm\" is choosen by default\n"
-    "\tIf -e,--edit is specified, \"uk\" is choosen by default\n"
-    "\tIf -p,--pattern is specified (and not -e,--edit), \"ac\" is choosen by default\n"
-    "\tIf -m,--clmode is enabled (and not -e,--edit) \"bf\" is choosen by default\n"
-    "\n"
-    "Special conditions:\n"
-    "\tIf -e,--edit is specified, an exact algorithm can not be choosen\n"
-    "\tIf -m,--clmode is enabled, only \"bf\" can be choosen\n"
-    "\n"
-    "\n"
-    "-e,--edit <arg>      Specify the edit distance for approx algorithms\n"
-    "arg should be positive\n"
-    "Special conditions:\n"
-    "\tIf -a,--algorithm sets an exact algorithm, -e,--edit can not be specified\n"
-    "\tIf -a,--algorithm sets an approx algorithm, -e,--edit should be specified\n"
-    "\n"
-    "\n"
-    "-c,--count           Display only the number of found occurrences\n"
-    "\n"
-    "\n"
-    "-p,--pattern <arg>   Specify a file with patterns to search\n";
+        "Usage:\n"
+                "ptm [<option>] pattern textfile [textfile...]  ->  search files\n"
+                "ptm [<option>] pattern  ->  search input stream\n"
+                "ptm <option> <-h | --help>  ->  show global help\n"
+                "ptm <-h | --help>  -> show option help\n"
+                "Options:\n"
+                "-h,--help            Display this information\n"
+                "-a,--algorithm <arg> Specify the search algorithm\n"
+                "-e,--edit <arg>      Specify the edit distance (required) for approx algorithms\n"
+                "-c,--count           Display only the number of found occurrences\n"
+                "-p,--pattern <arg>   Specify a file with patterns to search\n"
+                "                     If specified, the first global argument (pattern) is treated text file\n"
+                "\n"
+                "Examples:\n"
+                "ptm -algorithm kmp -c abra abracadrabra.txt\n"
+                "\n"
+                "\n"
+                "-a,--algorithm <arg> Specify the search algorithm\n"
+                "arg options:\n"
+                "\texact: bf, kmp, bm, ac, so\n"
+                "\tapprox: se, uk, wu\n"
+                "\n"
+                "If algorithm is not specified:\n"
+                "\tIf nothing (except -c,--count) is specified, \"bm\" is choosen by default\n"
+                "\tIf -e,--edit is specified, \"uk\" is choosen by default\n"
+                "\tIf -p,--pattern is specified (and not -e,--edit), \"ac\" is choosen by default\n"
+                "\tIf -m,--clmode is enabled (and not -e,--edit) \"bf\" is choosen by default\n"
+                "\n"
+                "Special conditions:\n"
+                "\tIf -e,--edit is specified, an exact algorithm can not be choosen\n"
+                "\tIf -m,--clmode is enabled, only \"bf\" can be choosen\n"
+                "\n"
+                "\n"
+                "-e,--edit <arg>      Specify the edit distance for approx algorithms\n"
+                "arg should be positive\n"
+                "Special conditions:\n"
+                "\tIf -a,--algorithm sets an exact algorithm, -e,--edit can not be specified\n"
+                "\tIf -a,--algorithm sets an approx algorithm, -e,--edit should be specified\n"
+                "\n"
+                "\n"
+                "-c,--count           Display only the number of found occurrences\n"
+                "\n"
+                "\n"
+                "-p,--pattern <arg>   Specify a file with patterns to search\n";
 
-bool isInVector(const string &text, vector<string> vector)
-{
+bool isInVector(const string &text, vector<string> vector) {
     for (auto &textInVector : vector)
         if (text == textInVector)
             return true;
     return false;
 }
 
-CliOptions parseCommand(int argc, char **argv)
-{
+CliOptions parseCommand(int argc, char **argv) {
     CliOptions options = {};
     int optionFlag, uselessIndex;
-    while ((optionFlag = getopt_long(argc, argv, shortCliOptions.c_str(), &longCliOptions[0], &uselessIndex)) != -1)
-    {
-        switch (optionFlag)
-        {
-        case 'h':
-            cout << help;
-            exit(0);
-        case 'a':
-            options.algorithm = optarg;
-            options.isExact = isInVector(optarg, exactAlgorithmArguments);
-            options.isApprox = isInVector(optarg, approxAlgorithmArguments);
-            if (!options.isExact && !options.isApprox)
-            {
-                cerr << "ERR: illegal algorithm name" << endl;
-                exit(1);
+    while ((optionFlag = getopt_long(argc, argv, shortCliOptions.c_str(), &longCliOptions[0], &uselessIndex)) != -1) {
+        switch (optionFlag) {
+            case 'h':
+                cout << help;
+                exit(0);
+            case 'a':
+                options.algorithm = optarg;
+                options.isExact = isInVector(optarg, exactAlgorithmArguments);
+                options.isApprox = isInVector(optarg, approxAlgorithmArguments);
+                if (!options.isExact && !options.isApprox) {
+                    cerr << "ERR: illegal algorithm name" << endl;
+                    exit(1);
+                }
+                break;
+            case 'e':
+                options.editDistance = stoi(optarg);
+                if (options.editDistance < 0) {
+                    cerr << "ERR: edit distance should be positive" << endl;
+                    exit(1);
+                }
+                break;
+            case 'c':
+                options.count = true;
+                break;
+            case 'p': {
+                ifstream patternsFileStream(optarg);
+                if (!patternsFileStream.is_open()) {
+                    cerr << "ERR: can not open patterns file" << endl;
+                    exit(1);
+                }
+                for (string line; getline(patternsFileStream, line);)
+                    options.patterns.push_back(line);
+                patternsFileStream.close();
             }
-            break;
-        case 'e':
-            options.editDistance = stoi(optarg);
-            if (options.editDistance < 0)
-            {
-                cerr << "ERR: edit distance should be positive" << endl;
+                break;
+            default:
+                cerr << "ERR: option not found" << endl;
                 exit(1);
-            }
-            break;
-        case 'c':
-            options.count = true;
-            break;
-        case 'p':
-            options.patternFile = optarg;
-            break;
-        default:
-            cerr << "ERR: option not found" << endl;
-            exit(1);
         }
     }
 
-    //Default options
-    if (options.algorithm.empty())
-    {
-        if (options.editDistance != -1)
-        {
+    // Default options
+    if (options.algorithm.empty()) {
+        if (options.editDistance != -1) {
             options.algorithm = "uk";
             options.isApprox = true;
-        }
-        else if (!options.patternFile.empty())
-        {
+        } else if (options.patterns.size() > 1) {
             options.algorithm = "ac";
             options.isExact = true;
-        }
-        else
-        {
+        } else {
             options.algorithm = "bm";
             options.isExact = true;
         }
     }
 
-    // Checking conflicts
+    // Check options conflicts
     if ((options.isExact && options.editDistance != -1) || (options.isApprox && options.editDistance == -1))
         cerr << "ERR: conflicting options, -a,--algorithm and -e,--edit, check --help" << endl;
 
-    // Global arguments
+    // Get global arguments
     char **arguments = argv + optind;
     int argumentCount = argc - optind;
-    for (int i = 0; i < argumentCount; ++i)
-        options.arguments.emplace_back(arguments[i]);
-
+    if (argumentCount == 0 && options.patterns.empty()) {
+        cerr << "ERR: missing pattern" << endl;
+        exit(1);
+    }
+    for (int i = 0; i < argumentCount; i++)
+        if (i == 0 && options.patterns.empty()) options.patterns.emplace_back(arguments[i]);
+        else options.files.emplace_back(arguments[i]);
     return options;
 }

7c750bcb47461312e67b2e88c4ad65ca866fb478
gaabs <gaabs@cin.ufpe.br>
2017-10-10 14:40:01 -0300

    Wildcard fix

--- src/dirtest.cpp
+++ src/dirtest.cpp
@@ -3,12 +3,19 @@
 #include <string>
 #include <iostream>
 #include <set>
+#include <map>
 
 using namespace std;
+
+map<pair<int,int>, bool > findMemo;
 
 bool findWildcard(string fileName, string fileRegex, int fileNamePos, int fileRegexPos){
     if (fileNamePos == fileName.length() && fileRegexPos == fileRegex.length()) return true;
     if (fileNamePos == fileName.length() || fileRegexPos == fileRegex.length()) return false;
+
+    pair<int,int> posPair = pair<int,int>(fileNamePos, fileRegexPos);
+    map<pair<int,int>, bool >::iterator findMemoIt = findMemo.find(posPair);
+    if (findMemoIt != findMemo.end()) return findMemoIt -> second;
 
     bool found = false;
     if (fileName[fileNamePos] == fileRegex[fileRegexPos]) {
@@ -21,7 +28,14 @@ bool findWildcard(string fileName, string fileRegex, int fileNamePos, int fileRe
         found |= findWildcard(fileName, fileRegex, fileNamePos, fileRegexPos + 1);
     }
 
+    findMemo[posPair] = found;
     return found;
+}
+
+bool findWildcard(string fileName, string fileRegex){
+  bool found = findWildcard(fileName, fileRegex, 0, 0);
+  findMemo.clear();
+  return found;
 }
 
 void searchFiles(string fullPath, set<string> &fileSet){
@@ -41,7 +55,7 @@ void searchFiles(string fullPath, set<string> &fileSet){
         tinydir_file file;
         tinydir_readfile(&dir, &file);
 
-        if (!file.is_dir && findWildcard(file.name, fileRegex, 0, 0)) {
+        if (!file.is_dir && findWildcard(file.name, fileRegex)) {
             printf("%s\n", file.name);
             fileSet.insert(dirPath + file.name);
         }

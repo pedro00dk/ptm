d6ee1f18a2820b84c943e5ace3ce85e0f93fa511
gaabs <gaabs@cin.ufpe.br>
2017-10-12 13:32:05 -0300

    Add simple correctness check

--- benchmarks/benchmark_exact_single.py
+++ benchmarks/benchmark_exact_single.py
@@ -1,3 +1,4 @@
+import re
 import subprocess
 import timeit
 
@@ -6,23 +7,37 @@ from matplotlib import pyplot
 
 
 def compile():
-    command = "g++ ../src/main.cpp -std=c++11 -O2 -o pmt"
+    command = "g++ ../src/ptm.cpp -std=c++11 -O2 -o pmt"
     subprocess.call(command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
 
 
 def run(pattern, text_file, algorithm, has_pattern_file=False):
     if algorithm == "grep":
-        command = 'grep {0} {1} -c'.format(pattern,
+        command = 'grep {0} {1} -o | wc -l'.format(pattern,
                                            text_file)
     else:
         command = './pmt {0} {1} {2} -a {3} -c'.format("-p" if has_pattern_file else "",
                                                        pattern,
                                                        text_file,
                                                        algorithm)
+
     proc = subprocess.Popen(command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
     proc.wait()
 
-    return proc
+    return proc.stdout.read()
+
+
+def get_execution_time(pattern, text_file, algorithm, number_executions):
+    timer = timeit.Timer(lambda: run(pattern, text_file, algorithm))
+    time = timer.timeit(number=number_executions) / number_executions
+    return time
+
+
+def get_occurrences_count(pattern, text_file, algorithm):
+    output_str = run(pattern, text_file, algorithm).decode('ascii')
+    occurrences_count = re.search(r'\d+', output_str).group()
+
+    return occurrences_count
 
 
 def main():
@@ -35,28 +50,44 @@ def main():
     number_executions = 10
     max_pattern_len = 30
 
-    results = {}
+    # assert correctness
+    for text_file in text_files:
+        for pattern in patterns:
+            occurrences_counts = {}
+            for algorithm in exact_algorithms:
+                occurrences_count = get_occurrences_count(pattern, text_file, algorithm)
+                if occurrences_count not in occurrences_counts:
+                    occurrences_counts[occurrences_count] = [algorithm]
+                else:
+                    occurrences_counts[occurrences_count].append([algorithm])
+
+            if len(occurrences_counts) != 1:
+                print(occurrences_counts)
+
+            assert len(occurrences_counts) == 1
+
+    # calculate execution times
+    execution_times = {}
 
     for algorithm in exact_algorithms:
-        results[algorithm] = [0] * (max_pattern_len + 1)
+        execution_times[algorithm] = [0] * (max_pattern_len + 1)
 
     for text_file in text_files:
         for pattern in patterns:
             pattern_len = len(pattern)
             for algorithm in exact_algorithms:
-                timer = timeit.Timer(lambda: run(pattern, text_file, algorithm))
-                time = timer.timeit(number=number_executions) / number_executions
+                time = get_execution_time(pattern, text_file, algorithm, number_executions)
                 print(algorithm, pattern_len, time)
-                results[algorithm][pattern_len] += time
+                execution_times[algorithm][pattern_len] += time
 
     # plot results
-    for (algorithm, algorithm_results) in results.items():
+    for (algorithm, algorithm_execution_times) in execution_times.items():
         x = []
         y = []
-        for (pattern_len, result) in enumerate(algorithm_results):
-            if result > 0:
+        for (pattern_len, execution_time) in enumerate(algorithm_execution_times):
+            if execution_time > 0:
                 x.append(pattern_len)
-                y.append(result)
+                y.append(execution_time)
 
         pyplot.plot(x, y, label=algorithm)
 

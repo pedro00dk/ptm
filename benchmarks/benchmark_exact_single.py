bd5eb0425d6eee801f458f9130e6ddc8ed8d698c
Pedro Henrique <pedro00dk@gmail.com>
2017-10-13 13:49:12 -0300

    Benchmark script now tests exact and approx algorithms
    To fix -> clear plt after first plot.

--- benchmarks/benchmark_exact_single.py
+++ benchmarks/benchmark_exact_single.py
@@ -24,18 +24,18 @@ def get_text_patterns(text_file, pattern_sizes):
     return patterns
 
 
-def run(algorithm, pattern, text_file):
+def run(algorithm, pattern, text_file, edit=-1):
     if algorithm == "grep":
         command = 'grep "%s" "%s" -o | wc -l' % (pattern, text_file)
     else:
-        command = './ptm -a %s -c "%s" "%s"' % (algorithm, pattern, text_file)
+        command = './ptm -a %s %s -c "%s" "%s"' % (algorithm, '' if edit == -1 else '-e %d' % edit, pattern, text_file)
     process = subprocess.Popen(command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
     process.wait()
     return process.stdout.read()
 
 
-def get_run_time(algorithm, pattern, text_file, runs):
-    timer = timeit.Timer(lambda: run(algorithm, pattern, text_file))
+def get_run_time(runs, algorithm, pattern, text_file, edit=-1):
+    timer = timeit.Timer(lambda: run(algorithm, pattern, text_file, edit))
     time = timer.timeit(number=runs) / runs
     return time
 
@@ -43,8 +43,8 @@ def get_run_time(algorithm, pattern, text_file, runs):
 def main():
     gen_text_source = 'gentext.c'
     gen_text = 'gentext.o'
-    text_file = 'text.txt'
-    text_size = 100
+    text_file = 'text_exact.txt'
+    text_size = 1000
     text_sigma = 255
     compile_command = 'gcc -O2 %s -o %s' % (gen_text_source, gen_text)
     subprocess.call(compile_command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
@@ -76,7 +76,42 @@ def main():
     for algorithm in exact_algorithms:
         run_times[algorithm] = []
         for pattern in patterns:
-            time = get_run_time(algorithm, pattern, text_file, runs)
+            time = get_run_time(runs, algorithm, pattern, text_file)
+            print('algorithm %s, pattern length %d, mean run time %f' % (algorithm, len(pattern), time))
+            run_times[algorithm].append(time)
+        print()
+
+    x = [len(pattern) for pattern in patterns]
+    for algorithm, results in run_times.items():
+        plt.plot(x, results, label=algorithm)
+    plt.xlabel('pattern size')
+    plt.ylabel('time (sec)')
+    plt.legend()
+    plt.show()
+    plt.cla()
+
+    text_file = 'text_approx.txt'
+    text_size = 250
+    run_command = './%s %s %d %d' % (gen_text, text_file, text_size, text_sigma)
+    subprocess.call(run_command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+    print('text file -> name: %s, size: %d mb, ab size: %d' % (text_file, text_size, text_sigma))
+
+    patterns = get_text_patterns(text_file, [*range(5, 101, 5)])
+    print('patterns -> count: %d:' % len(patterns))
+    for pattern in patterns:
+        print('(%d): %s' % (len(pattern), pattern))
+
+    approx_algorithms = ['se', 'uk']
+    print('algorithms:')
+    print(approx_algorithms)
+
+    run('grep', patterns[0], text_file)  # Just to load file
+
+    print('testing algorithms in %d runs' % runs)
+    for algorithm in approx_algorithms:
+        run_times[algorithm] = []
+        for pattern in patterns:
+            time = get_run_time(runs, algorithm, pattern, text_file, 0)
             print('algorithm %s, pattern length %d, mean run time %f' % (algorithm, len(pattern), time))
             run_times[algorithm].append(time)
         print()

3b7c4d52bcb5f45965940424430fbb86acdbe97a
gaabs <gaabs@cin.ufpe.br>
2017-10-14 13:48:01 -0300

    Refactor tests and add pattern file test

--- benchmarks/benchmarks_plots.py
+++ benchmarks/benchmarks_plots.py
@@ -1,6 +1,7 @@
 import random
 import subprocess
 import timeit
+from operator import itemgetter
 
 import matplotlib.pyplot as plt
 
@@ -24,11 +25,17 @@ def get_text_patterns(text_file, pattern_sizes):
     return patterns
 
 
-def run(algorithm, pattern, text_file, edit=-1):
+def run(algorithm, pattern, text_file, edit=-1, pattern_file=False):
     if algorithm == "grep":
-        command = 'grep "%s" "%s" -o | wc -l' % (pattern, text_file)
+        command = 'grep %s "%s" "%s" -o | wc -l' % ('-f' if pattern_file else '',
+                                                    pattern,
+                                                    text_file)
     else:
-        command = './ptm -a %s %s -c "%s" "%s"' % (algorithm, '' if edit == -1 else '-e %d' % edit, pattern, text_file)
+        command = './ptm -a %s %s -c %s "%s" "%s"' % (algorithm,
+                                                      '' if edit == -1 else '-e %d' % edit,
+                                                      '-p' if pattern_file else '',
+                                                      pattern,
+                                                      text_file)
     process = subprocess.Popen(command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
     process.wait()
     x = process.stdout.read()
@@ -36,8 +43,8 @@ def run(algorithm, pattern, text_file, edit=-1):
     return x
 
 
-def get_run_time(runs, algorithm, pattern, text_file, edit=-1):
-    timer = timeit.Timer(lambda: run(algorithm, pattern, text_file, edit))
+def get_run_time(runs, algorithm, pattern, text_file, edit=-1, pattern_file=False):
+    timer = timeit.Timer(lambda: run(algorithm, pattern, text_file, edit, pattern_file))
     time = timer.timeit(number=runs) / runs
     return time
 
@@ -46,7 +53,7 @@ def choose_sample(sample):
     text_file = {}
     patterns = {}
 
-    text_file['english'] = 'tests/english.txt'
+    text_file['english'] = 'samples/english.txt'
     patterns['english'] = [
         'by', 'this', 'compose', 'footnote', 'appendices', 'word-processing', 'Provisional Government',
         'The Committee for Salvation', 'But the reader must realize that',
@@ -60,7 +67,7 @@ def choose_sample(sample):
         'cultivation of literature. Mencius tells us how the princes had made away with many of the records'
     ]
 
-    text_file['sources'] = 'tests/sources.txt'
+    text_file['sources'] = 'samples/sources.txt'
     patterns['sources'] = [
         'if', 'void', 'include', 'asm/irq.h', 'static u16', 'unsigned long', 'SERIAL_21285_NAME',
         'struct uart_port *port', 'static irqreturn_t serial21285',
@@ -73,7 +80,7 @@ def choose_sample(sample):
         'fh_compose(struct svc_fh *fhp, struct svc_export *exp, struct dentry *dentry, struct svc_fh',
         'int direct2indirect (struct reiserfs_transaction_handle *, struct inode *, struct path *, struct'
     ]
-    text_file['proteins'] = 'tests/proteins.txt'
+    text_file['proteins'] = 'samples/proteins.100MB'
     patterns['proteins'] = [
         'PQ', 'MAAL', 'PPPRPD', 'HDPAGALQ', 'HDPAGALQAA', 'EEEEEEEEATDSE', 'SLTTVPQTQGQTTAQKV',
         'FCDVTIVADSLKFRGHKVIL', 'DDKMLRFVDKNGLVPSSSGTVYDRTTVLIE',
@@ -88,7 +95,8 @@ def choose_sample(sample):
     ]
     return text_file[sample], patterns[sample]
 
-def test_and_plot_exact(text_file, patterns, exact_algorithms, runs):
+
+def run_and_plot_exact(text_file, patterns, exact_algorithms, runs):
     run_times = {}
 
     run('grep', patterns[0], text_file)  # Just to load file
@@ -111,7 +119,48 @@ def test_and_plot_exact(text_file, patterns, exact_algorithms, runs):
     plt.grid()
     plt.show()
 
-def test_and_plot_aprox(text_file, patterns, approx_algorithms, runs, max_edit_distance):
+
+def run_and_plot_exact_pattern_file(text_file, patterns, exact_algorithms, runs):
+    run_times = {}
+
+    patterns = patterns[:2]
+
+    patterns_filename = 'patterns.txt'
+    with open(patterns_filename, 'w') as patterns_file:
+        for pattern in patterns:
+            patterns_file.write(pattern)
+            patterns_file.write('\n')
+
+    run('grep', patterns[0], text_file)  # Just to load file
+
+    print('testing algorithms in %d runs' % runs)
+    for algorithm in exact_algorithms:
+        run_times[algorithm] = []
+        time = get_run_time(runs, algorithm, patterns_filename, text_file, pattern_file=True)
+        print('algorithm %s, pattern file, mean run time %f' % (algorithm, time))
+        run_times[algorithm].append(time)
+        print()
+
+    print(run_times)
+    x = list(range(len(exact_algorithms)))
+    x_names = []
+    y = []
+
+    for (algorithm, result) in sorted(run_times.items(), key=itemgetter(1), reverse=True):
+        x_names.append(algorithm)
+        y.append(result)
+
+    plt.xticks(x, x_names)
+    plt.xlabel('algorithm')
+    plt.ylabel('time (sec)')
+    plt.legend()
+    plt.grid()
+    plt.plot(x, y, 'g')
+    plt.show()
+    # TODO: plotar barchart
+
+
+def run_and_plot_aprox(text_file, patterns, approx_algorithms, runs, max_edit_distance):
     run_times = {}
 
     run('grep', patterns[0], text_file)  # Just to load file
@@ -135,7 +184,7 @@ def test_and_plot_aprox(text_file, patterns, approx_algorithms, runs, max_edit_d
 
 
 def main():
-    text_file, patterns = choose_sample('english')
+    text_file, patterns = choose_sample('proteins')
 
     print('patterns -> count: %d:' % len(patterns))
     for pattern in patterns:
@@ -151,17 +200,18 @@ def main():
     print('algorithms:')
     print(exact_algorithms)
 
-    runs = 4
-    test_and_plot_exact(text_file, patterns, exact_algorithms, runs)
+    runs = 1
+    run_and_plot_exact(text_file, patterns, exact_algorithms, runs)
+    run_and_plot_exact_pattern_file(text_file, patterns, exact_algorithms, runs)
 
     approx_algorithms = ['se', 'uk']
     print('algorithms:')
     print(approx_algorithms)
 
-    runs = 4
+    runs = 1
     max_edit_distance = 2
     for edit_distance in range(max_edit_distance + 1):
-        test_and_plot_aprox(text_file, patterns, approx_algorithms, runs, edit_distance)
+        run_and_plot_aprox(text_file, patterns, approx_algorithms, runs, edit_distance)
 
 
 if __name__ == "__main__":
